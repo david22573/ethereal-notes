4fae88219d3cb0095494226a4a164e715a376171
David Rivero <davidmiguel22573@gmail.com>
2025-02-27 18:40:42 -0800

    chore: setup database initlization in app lifespan

--- app/main.py
+++ app/main.py
@@ -1,3 +1,16 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
-app = FastAPI()
+from app.database import engine
+from app.models import create_db_and_tables
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables(engine)
+    yield
+    engine.dispose()
+
+
+app = FastAPI(lifespan=lifespan)
